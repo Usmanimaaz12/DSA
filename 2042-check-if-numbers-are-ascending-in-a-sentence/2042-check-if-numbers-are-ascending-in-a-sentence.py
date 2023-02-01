e7daa643ab72e0564ebb8de43b44600bd1938c76
MAAZ USMANI <90022701+Usmanimaaz12@users.noreply.github.com>
2023-02-02 00:58:13 +0530

    Time: 22 ms (98.62%), Space: 14 MB (10.80%) - LeetHub

---
+++ 2042-check-if-numbers-are-ascending-in-a-sentence/2042-check-if-numbers-are-ascending-in-a-sentence.py
@@ -0,0 +1,9 @@
+class Solution:
+    def areNumbersAscending(self, s: str) -> bool:
+        prev = -1
+        for i in s.split():
+            if i.isdigit():
+                if int(i)<=prev:
+                    return False
+                prev = int(i)
+        return True
