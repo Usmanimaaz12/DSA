c55e88401f6d233154bfaf80301f446a47249c88
MAAZ USMANI <90022701+Usmanimaaz12@users.noreply.github.com>
2023-02-18 22:34:03 +0530

    Update 0048-rotate-image.py

--- 0048-rotate-image/0048-rotate-image.py
+++ 0048-rotate-image/0048-rotate-image.py
@@ -1,9 +1,11 @@
 class Solution:
     def rotate(self, matrix: List[List[int]]) -> None:
+#         step-1: Reverse each row
         for row in matrix:
             row.reverse()
         m=len(matrix)
         n=len(matrix[0])
+#         Transpose along the positive diagonal
         for i in range(m-1):
             for j in range(m-i-1):
                 matrix[i][j],matrix[m-j-1][n-i-1]=matrix[m-j-1][n-i-1],matrix[i][j]
