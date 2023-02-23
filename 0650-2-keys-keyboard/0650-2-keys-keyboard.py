8203f696f4755ac2d514cf08236bd42838cd62aa
MAAZ USMANI <90022701+Usmanimaaz12@users.noreply.github.com>
2023-02-23 23:45:27 +0530

    Time: 43 ms (64.94%), Space: 13.8 MB (65.54%) - LeetHub

---
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.py
@@ -0,0 +1,9 @@
+class Solution:
+    def minSteps(self, n: int) -> int:
+        ans=0
+        for i in range(2,n+1):
+            while n%i==0:
+                ans+=i
+                n=n//i
+        return ans
+        
