bfb6977e72b46f7f1082739a96e60a279ba8e8a7
MAAZ USMANI <90022701+Usmanimaaz12@users.noreply.github.com>
2023-04-03 22:10:41 +0530

    Time: 30 ms (85.80%), Space: 14 MB (14.49%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
@@ -6,26 +6,17 @@
 #         self.right = right
 class Solution:
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
-        if not root:
-            return root
-        ans=[]
-        q=deque([None,root])
-        
-#         our answer will include last element of each level
-        while q:
-            x=q.pop()
-            if x:
-#                 applying condition to check that is it last element
-                if q[-1] is None:
-                    ans.append(x.val)
-                if x.left:
-                    q.appendleft(x.left)
-                if x.right:
-                    q.appendleft(x.right)
-            else:
-                if q:
-                    q.appendleft(None)
-        return ans
+        mp={}
+        def traverse(root,level):
+            if root:
+                # print(level)
+                mp[level]=root.val
+                traverse(root.left,level+1)
+                traverse(root.right,level+1)
+            
+        traverse(root,0)
+        return mp.values()
+                
             
             
             
