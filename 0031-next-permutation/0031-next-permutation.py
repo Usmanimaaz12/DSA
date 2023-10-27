b9b91894bce067ad55633292f85ef37217ccfe68
MAAZ USMANI <90022701+Usmanimaaz12@users.noreply.github.com>
2023-10-27 20:43:12 +0530

    Time: 46 ms (68.93%), Space: 16 MB (96.94%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.py
@@ -0,0 +1,27 @@
+class Solution:
+    def nextPermutation(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+#         length of nums
+        n=len(nums)
+
+        index = -1
+#         finding breakpoint from right side 
+        for i in range(n-2,-1,-1):
+            if nums[i] < nums[i+1]:
+                index = i
+                break
+#       if no breakpoint reverse and return 
+        if index == -1 :
+            nums.reverse()
+            return nums
+#         if breakpoint : in right half find smallest from right end and swap with index
+        for i in range(n-1,index,-1):
+            if nums[i]>nums[index]:
+                nums[i],nums[index] = nums[index], nums[i]
+                break
+# reverse the array from the index and return ans
+        nums[index+1:] = reversed(nums[index+1:])
+        return nums
+        
